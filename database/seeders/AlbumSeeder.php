b5030f2ab7b0f1233b3c4a7cdab8a42a6ef9f907
Drash Nayak <drashtantnayak@dal.ca>
2022-06-17 18:50:07 -0300

    added data in database tables

---
+++ database/seeders/AlbumSeeder.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Album;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AlbumSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Album::truncate();
+        $user = User::where('email', 'nick.reynolds@domain.co')->first();
+        $albums = [
+            [
+                "user_id" => $user->id,
+                "title" => "Nandhaka Pieris",
+                "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img" => "img/landscape1.jpg",
+                "created_at" => "2015-05-01",
+                "featured" => true
+            ],
+            [
+                "user_id" => $user->id,
+                "title" => "New West Calgary",
+                "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img" => "img/landscape2.jpg",
+                "created_at" => "2016-05-01",
+                "featured" => false
+            ],
+            [
+                "user_id" => $user->id,
+                "title" => "Australian Landscape",
+                "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img" => "img/landscape3.jpg",
+                "created_at" => "2015-02-02",
+                "featured" => false
+            ],
+            [
+                "user_id" => $user->id,
+                "title" => "Halvergate Marsh",
+                "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img" => "img/landscape4.jpg",
+                "created_at" => "2014-04-01",
+                "featured" => true
+            ],
+            [
+                "user_id" => $user->id,
+                "title" => "Rikkis Landscape",
+                "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img" => "img/landscape5.jpg",
+                "created_at" => "2010-09-01",
+                "featured" => false
+            ],
+            [
+                "user_id" => $user->id,
+                "title" => "Kiddi Kristjans Iceland",
+                "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                "img" => "img/landscape6.jpg",
+                "created_at" => "2015-07-21",
+                "featured" => true
+            ]
+        ];
+        foreach ($albums as $album) {
+            Album::create($album);
+        }
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        User::create([
+            'name' => "Nick Reynolds",
+            "phone" => "555-555-5555",
+            "email" => "nick.reynolds@domain.co",
+            "bio" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+            "profile_picture" => "img/profile.jpg",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
